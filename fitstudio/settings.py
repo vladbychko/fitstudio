9e47315b8f2fbb9b1ef6e5270d301a253cab5016
vladbychko <vladbbb856156@gmail.com>
2025-11-28 00:23:47 +0200

    Move static folder to root static dir

--- fitstudio/settings.py
+++ fitstudio/settings.py
@@ -115,9 +115,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+
